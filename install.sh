dc1d91a50c8493531916b491320527512dadc60a
faielli <federicoaielli.fa@gmail.com>
2026-07-18 01:04:49 +0200

    .

--- install.sh
+++ install.sh
@@ -8,16 +8,28 @@ PKGNAME="niripad"
 
 echo "==> Installazione di Niripad ${PKGVER}"
 
-# Crea il tarball dai file .py nella stessa cartella della repo
+# Crea una cartella temporanea con la struttura corretta
+TMPDIR=$(mktemp -d)
+SRCDIR="${TMPDIR}/${PKGNAME}-${PKGVER}"
+mkdir -p "${SRCDIR}"
+
+# Copia i file .py nella cartella temporanea
+cp "${PKGBUILD_DIR}"/*.py "${SRCDIR}/"
+
+# Crea il tarball dalla cartella temporanea
 echo "==> Creazione del tarball ${PKGNAME}-${PKGVER}.tar.gz..."
 tar -czf "${PKGBUILD_DIR}/${PKGNAME}-${PKGVER}.tar.gz" \
-    --exclude='__pycache__' \
-    --exclude='*.pyc' \
-    --transform "s|^\.|${PKGNAME}-${PKGVER}|" \
-    -C "${PKGBUILD_DIR}" \
-    $(ls "${PKGBUILD_DIR}"/*.py)
+    -C "${TMPDIR}" \
+    "${PKGNAME}-${PKGVER}/"
+
+# Pulizia
+rm -rf "${TMPDIR}"
 
 echo "==> Tarball creato."
+
+# Verifica
+echo "==> Contenuto del tarball:"
+tar -tzf "${PKGBUILD_DIR}/${PKGNAME}-${PKGVER}.tar.gz"
 
 # Build e installazione
 cd "$PKGBUILD_DIR"
